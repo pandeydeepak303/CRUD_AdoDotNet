bb487e0179c6f4e370bedbabcfd6cf358e892689
DeepakPandey <pandeydeepak704@gmail.com>
2023-11-03 12:28:05 +0530

    Add project files.

---
+++ AdoDotNet/ADO_CRUD.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDotNet
+{
+    public class ADO_CRUD
+    {
+       private readonly string con = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+
+        public ADO_CRUD(string con)
+        {
+            this.con = con;
+        }
+        public void ShowDepartment()
+        {     
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(con))
+                {
+                    string query = "Select * from Department where IsDeleted=0";
+                    SqlCommand cmd = new SqlCommand(query, myConnection);
+                    myConnection.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var sno = reader["sno"].ToString();
+                            var Department = reader["Department"].ToString();
+                            Console.WriteLine(sno + " " + Department);
+                        }
+                        myConnection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+        public void ShowEmployeeList()
+        {
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(con))
+                {
+                    string query = "SELECT e.Name, d.Department, e.salary, e.age " +
+                                   "FROM Employee e " +
+                                   "LEFT JOIN Department d ON e.DepartmentId = d.sno " +
+                                   "WHERE d.IsDeleted = 0";
+
+                    SqlCommand cmd = new SqlCommand(query, myConnection);
+                    myConnection.Open();
+                    Console.WriteLine("Employee Name\t" + "age\t\t" + "salary\t\t" + "Department");
+                    Console.WriteLine("-----------------------------------------------------------");
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var employeeName = reader["Name"].ToString();
+                            var department = reader["Department"].ToString();
+                            var age = reader["age"].ToString();
+                            var salary = reader["salary"].ToString();
+
+                            Console.WriteLine(employeeName + "\t\t" + age + "\t\t" + salary + "\t" + department);
+                        }
+                        Console.WriteLine("-----------------------------------------------------------");
+                        myConnection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        public void AddDepartment()
+            {    
+                try
+                {
+                    Console.WriteLine(" Enter your Department Name ");
+                    string departmentName = Console.ReadLine();
+                              
+                  using (SqlConnection myConnection = new SqlConnection(con))
+                    {
+                        string query = "INSERT INTO Department (Department, IsDeleted) VALUES (@Department, 0)";
+                        SqlCommand cmd = new SqlCommand(query, myConnection);
+                        cmd.Parameters.AddWithValue("@Department", departmentName);
+                        myConnection.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            Console.WriteLine("Department added successfully.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to add department.");
+                        }
+                    }
+                               Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+
+        public void DeleteDepartment( )
+        {
+
+            Console.WriteLine("Enter your Id Which You Want To delete  ");
+            int sno = int.Parse(Console.ReadLine());        
+            try
+            {
+                using (SqlConnection myConnection = new SqlConnection(con))
+                {
+                    string query = "UPDATE Department SET IsDeleted = 1 WHERE sno = @sno";
+                    SqlCommand cmd = new SqlCommand(query, myConnection);
+                   cmd.Parameters.AddWithValue("@sno", sno);
+                    myConnection.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Department deleted successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to delete department.");
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ AdoDotNet/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDotNet
+{
+    public  class Program
+    {
+
+
+     public  static void Main(string[] args)
+        {
+            int choice;
+            string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+            ADO_CRUD aDO_CRUD = new ADO_CRUD(connectionString);
+            do
+            {
+                Console.WriteLine("EmployeeList--");
+                aDO_CRUD.ShowEmployeeList();
+            
+                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("Press 1 to add a department");
+                Console.WriteLine("Press 2 to show departments");
+                Console.WriteLine("Press 3 to delete a department");
+                Console.WriteLine("Press 0 to exit");
+
+                string userInput = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(userInput))
+                {
+                    Console.WriteLine("Invalid choice! Please try again.");
+                    continue;
+                }
+
+                if (!int.TryParse(userInput,  out choice))
+                {
+                    Console.WriteLine("Invalid choice! Please enter a valid integer.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        aDO_CRUD.AddDepartment();
+                        break;
+                    case 2:
+                        aDO_CRUD.ShowDepartment();
+                        break;
+                    case 3:
+                        aDO_CRUD.DeleteDepartment();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting the program...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+
+                Console.WriteLine("Do you want to continue? (y/n)");
+            } while (Console.ReadLine().ToLower() == "y");
+
+            Console.WriteLine("Program execution completed.");
+            Console.ReadLine();
+
+        }
+            }
+        }
+    
+
